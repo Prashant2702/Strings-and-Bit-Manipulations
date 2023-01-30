6d84bd084547f9fdb18047380a5017c31da8e942
Prashant2702 <109785759+Prashant2702@users.noreply.github.com>
2023-01-30 12:24:01 +0530

    Finding a unique number.

---
+++ unique1.cpp
@@ -0,0 +1,24 @@
+//Write a program to find a unique number in an array where all numbers except
+//one, are present twice.
+#include<iostream>
+using namespace std;
+
+int unique( int arr[], int n){
+   int xorsum=0;
+   for (int i=0; i < n; i++)
+   {
+    for(int i=0;i<n;i++){
+        xorsum = xorsum^arr[i];
+    }
+    /* code */
+    return xorsum;
+   }
+    
+}
+
+int main(){
+
+int arr = {1,2,3,4,1,2,3};
+cout<< unique(arr,7)<<endl;
+    return 0;
+}
